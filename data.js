7b1a44bb6de4cd75bdddaf0c40d3cc509e2086f1
AnushkaSahu2003 <138781098+AnushkaSahu2003@users.noreply.github.com>
2024-08-02 01:25:33 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,67 @@
+const data = {
+    msc: [
+        {
+            category: "Rosso Veneto",
+            artist: "Discover"
+        },
+        {
+            category: "Corvina",
+            artist: "Discover"
+        },
+        {
+            category: "Bianco Veneto",
+            artist: "Discover"
+        },
+    ],
+    scan: [
+        {
+            title: "Red passionate",
+            description: "The first Amicale to initiate the tradition was a full-bodled red passionate like an emotion and noble like statement",
+            position: "left-0 bottom-[20vh]",
+            maskPosition: "14% at 57.5% 27%"
+        },
+        {
+            title: "Esence of the land",
+            description: "every drop of this wine encapsulates the essence of the land where it grows,the strength og the surrounding countryside,and the serenity of the lake.",
+            position: "left-0 top-[5rem]",
+            maskPosition: "14% at 53.5% 75%"
+        },
+        {
+            title: "Veronese territory",
+           
+            description: "In the heart of the Veronese territory,protected by the mountains and embraced by the lake.",
+            position: "right-0 top-[20vh] md:top-[40vh]",
+            maskPosition: "14% at 27.5% 70%"
+        },
+        {
+            title: "Unique terroir",
+            description: "The success of this line of wines is the result of a unique terroir,which combines the natural characteristics of the land.",
+            position: "left-0 top-[5rem]",
+            maskPosition: "14% at 60.5% 74%"
+        },
+        {
+            title: "Process",
+            description: "with an ancient winemaking process that involves the appassimento technique of graphes and a gentle maceration.",
+            position: "right-0 top-[65vh]",
+            maskPosition: "10% at 46% 48%"
+        },
+        {
+            title: "Production",
+            description: "Amicale's production recalls ancient traditions and rediscovers ancient flavors",
+            position: "right-0 top-[20vh]",
+            maskPosition: "11% at 14.5% 69%"
+        },
+        {
+            title: "",
+            description: "",
+            position: "",
+            maskPosition: "100% at 50% 50%"
+        },
+    ],
+    vids: {
+        thumbnail: [
+            "wine3.jpg",
+            "wine4.jpg"
+        ]
+    }
+}
